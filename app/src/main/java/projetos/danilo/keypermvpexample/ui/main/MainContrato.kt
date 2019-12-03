a3c6247875989b2fdde89d8bd7a9c5a65fc4aac0
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-12-03 17:14:51 -0300

    Interface MainContrato

---
+++ app/src/main/java/projetos/danilo/keypermvpexample/ui/main/MainContrato.kt
@@ -0,0 +1,13 @@
+package projetos.danilo.keypermvpexample.ui.main
+
+interface MainContrato {
+    //para realizar a ligação entre View e Presenter
+    interface View{
+
+    }
+
+    interface Presenter{
+
+    }
+
+}
