1869917615cde12733d77f7801647468aec6894c
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-12-05 07:11:41 -0300

    Lista frutas
    Listview para exibir a lista de frutas que é o único componente na tela
    ate o momento

--- app/src/main/java/projetos/danilo/keypermvpexample/ui/frutas/FrutasActivity.kt
+++ app/src/main/java/projetos/danilo/keypermvpexample/ui/frutas/FrutasActivity.kt
@@ -2,6 +2,7 @@ package projetos.danilo.keypermvpexample.ui.frutas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_frutas.*
 import projetos.danilo.keypermvpexample.R
 
 class FrutasActivity : AppCompatActivity() {
@@ -9,5 +10,9 @@ class FrutasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frutas)
+
+        val lista = lista_frutas
+
+
     }
 }
