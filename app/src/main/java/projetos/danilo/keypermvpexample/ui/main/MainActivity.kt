f71e760f5a9571a9d868e07670279d8e1c9899f4
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-12-04 23:25:22 -0300

    Itens da View
    EditText nome, senha e botão entrar referenciados na view MainActivity

--- app/src/main/java/projetos/danilo/keypermvpexample/ui/main/MainActivity.kt
+++ app/src/main/java/projetos/danilo/keypermvpexample/ui/main/MainActivity.kt
@@ -2,6 +2,8 @@ package projetos.danilo.keypermvpexample.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import projetos.danilo.keypermvpexample.R
 
 class MainActivity : AppCompatActivity(), MainContrato.View {
@@ -14,5 +16,13 @@ class MainActivity : AppCompatActivity(), MainContrato.View {
 
         //inicializa presenter que terá nossa lógica, this é dependência é a view da camada contrato
         presenter = MainPresenter(this)
+
+        val nome = et_username.text.toString()
+        val senha = et_password.text.toString()
+
+        btn_login.setOnClickListener{
+//            Toast.makeText(this,"Botão pressionado",Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
