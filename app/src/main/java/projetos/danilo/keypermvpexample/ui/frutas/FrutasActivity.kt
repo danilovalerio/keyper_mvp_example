3e72252aa298ae8cbe95099d3277e2a7d169e1ea
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-12-05 07:20:55 -0300

    Adapter
    Adapter com recursos do próprio android para exibir a lista

--- app/src/main/java/projetos/danilo/keypermvpexample/ui/frutas/FrutasActivity.kt
+++ app/src/main/java/projetos/danilo/keypermvpexample/ui/frutas/FrutasActivity.kt
@@ -1,13 +1,16 @@
 package projetos.danilo.keypermvpexample.ui.frutas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_frutas.*
 import projetos.danilo.keypermvpexample.R
 
 class FrutasActivity : AppCompatActivity(), FrutasContrato.View {
 
     private lateinit var presenter: FrutasPresenter
+    //adapter que contém uma lista de strings
+    private lateinit var adapter: ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +19,14 @@ class FrutasActivity : AppCompatActivity(), FrutasContrato.View {
         //iniciar presenter
         presenter = FrutasPresenter(this)
 
+        adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            android.R.id.text1,
+            arrayOf()
+        )
         val lista = lista_frutas
-
+        lista.adapter = adapter
 
     }
 }
