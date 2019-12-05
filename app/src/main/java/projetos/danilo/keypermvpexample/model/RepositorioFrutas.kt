d3b4becc30edda086c97baae316f7f3599d7e788
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-12-05 07:29:02 -0300

    Lista de frutas
    Lista de exemplo com o getFrutas que retorna a lista de frutas

--- app/src/main/java/projetos/danilo/keypermvpexample/model/RepositorioFrutas.kt
+++ app/src/main/java/projetos/danilo/keypermvpexample/model/RepositorioFrutas.kt
@@ -1,4 +1,17 @@
 package projetos.danilo.keypermvpexample.model
 
 class RepositorioFrutas {
+
+    private val frutas = arrayOf(
+        "Banana",
+        "Manga",
+        "Pêra",
+        "Pêssego",
+        "Uva",
+        "Laranja"
+    )
+
+    fun getFrutas(): Array<String>{
+        return frutas
+    }
 }
