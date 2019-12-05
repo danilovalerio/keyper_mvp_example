9dcdc27d2af480f6f76686ba724af6f6ec803e87
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-12-05 07:30:52 -0300

    Exibindo as frutas
    Passando para mostradados o exibe frutas

--- app/src/main/java/projetos/danilo/keypermvpexample/ui/frutas/FrutasPresenter.kt
+++ app/src/main/java/projetos/danilo/keypermvpexample/ui/frutas/FrutasPresenter.kt
@@ -1,7 +1,16 @@
 package projetos.danilo.keypermvpexample.ui.frutas
 
+import projetos.danilo.keypermvpexample.model.RepositorioFrutas
+
 class FrutasPresenter(private val view: FrutasContrato.View) : FrutasContrato.Presenter{
+
+    private val repositorioFrutas = RepositorioFrutas()
+
     override fun getFrutas() {
+
+        val frutas = repositorioFrutas.getFrutas()
+        //pega o método de visualização e passa frutas
+        view.mostraDados(frutas)
 
     }
 }
